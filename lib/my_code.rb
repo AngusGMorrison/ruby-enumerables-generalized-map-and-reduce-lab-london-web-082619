5eb0e4baa05259f4a48eaa6e3048b28ff0164d35
Angus Morrison <contact@angus-morrison.com>
2019-08-07 20:18:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+def map(array)
+  new_arr = []
+  array.each do |elem|
+    new_arr << yield(elem)
+  end
+  new_arr
+end
+
+def reduce(array, start=0)
+  array.each do |elem|
+    start = yield(start, elem)
+  end
+  start
+end
